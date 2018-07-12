e41acf2136698c3ba2edf64fed57792ebd748014
hungarima <tronghung199@gmail.comm>
2018-07-12 20:20:02 +0700

    up

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
 import MainScreen from './containers/MainScreen';
-<<<<<<< HEAD
-
 
 
 class App extends Component {
-
-=======
-
-class App extends Component {
-  
->>>>>>> c219dd6415281849cbd85f4262fb3805c9c28ece
   render() {
     return (
       <div className="App">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,15 +29,15 @@ class NavBar extends Component {
     }
 
     
-    like = () => {
+    // like = () => {
 
-        axios.post(`/api/url/${req.params.urlId}`)
-        .then(response => {
-            console.log(response);
+    //     axios.post(`/api/url/${req.params.urlId}`)
+    //     .then(response => {
+    //         console.log(response);
 
-        })
-        .catch(error => console.log(error));
-    }
+    //     })
+    //     .catch(error => console.log(error));
+    // }
 
     _onLogin = (submittedUsername, submittedPassword) => {
         axios
