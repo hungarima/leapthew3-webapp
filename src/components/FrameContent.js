15e9acd3144b5b180b30a74c4cd8031a03ba043e
anhtdam <tuananh1108.97@gmail.com>
2018-07-15 06:46:10 +0700

    loading

--- src/components/FrameContent.js
+++ src/components/FrameContent.js
@@ -2,22 +2,14 @@ import React, { Component } from 'react';
 import Iframe from 'react-iframe';
 import axios from '../axios';
 class FrameContent extends Component {
-    state = {
-        iframeSrc: '',
-    }
-
+    
     componentDidMount() {
-        const iframe = document.getElementById("frame");
-        console.log(iframe);
-        iframe.onload = () => {
-            console.log(this.state.iframeSrc);
-        }
         // will get called before first time render
         if (this.props.currentUrlId) {
             axios
                 .get(`api/url/${this.props.currentUrlId}/data`)
                 .then(response => {
-                    localStorage.setItem("currentUrlId", this.props.currentUrlId);
+                    localStorage.setItem("currentUrlId", this.props.currentUrlId );
                     this.setState({ iframeSrc: response.data.url });
                 })
                 .catch(err => {
@@ -27,11 +19,13 @@ class FrameContent extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
+        console.log(nextProps.currentUrlId);
+        this.setState({iframeSrc:null})
         // will get called before every time this component re-render
         axios
             .get(`api/url/${nextProps.currentUrlId}/data`)
             .then(response => {
-                localStorage.setItem("currentUrlId", this.props.currentUrlId);
+                localStorage.setItem("currentUrlId",this.props.currentUrlId );
                 this.setState({ iframeSrc: response.data.url });
             })
             .catch(err => {
@@ -39,33 +33,23 @@ class FrameContent extends Component {
             })
     }
 
-    renderIframe() {
+    state = { iframeSrc: null }
 
-        return (
-            <div>
-                <object id="frame" onLoad={() => { console.log("load") }} data={this.state.iframeSrc} width="100%" height="600" type="text/html"> </object>
-                <div id="loadingMessage">Loading...</div>
-            </div>
-
-            // return <Iframe
-            //     url={this.state.iframeSrc}
-            //     width="100%"
-            //     height="600px"
-            //     display="initial"
-            //     position="relative"
-            //     frameborder="0"
-            //     allowFullScreen
-            //     onbeforeunload={() => { console.log("123") }}
-            // >
-            // </Iframe>;
-
-        )
-    }
 
     render() {
+        if(!this.state.iframeSrc) return <div><img src="/assets/loading.gif"/></div>
         return (
-            <div className="frame">
-                {this.renderIframe()}
+            <div className="frame" >
+                <Iframe
+                    url={this.state.iframeSrc}
+                    width="100%"
+                    height="600px"
+                    display="initial"
+                    position="relative"
+                    frameborder="0"
+                    allowFullScreen
+                >
+                </Iframe>
             </div>
         );
     }
