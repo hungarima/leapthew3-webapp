6dc9de42cca2b09a2b58f30f27229ea479166459
eddienguyen <eddienguyen996@gmail.com>
2018-07-17 06:55:51 +0700

    signin & signup on simplenavbar

--- src/components/SimpleNavBar.js
+++ src/components/SimpleNavBar.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react';
-
+import axios from "../axios";
+import config from '../config';
 import {
     Navbar,
-    Button
+    Button,
+    NavLink
 } from 'reactstrap';
 import ProfilePanel from './ProfilePanel';
 
 class SimpleNavbar extends Component {
+
+    state = {
+        loginModalOpen: false
+    }
+
+    componentWillMount = () => {
+        axios.get("/api/auth").then(response => this.setState({
+            username: response.data.username,
+            id: response.data.id
+        }))
+    }
+
+    _onRegister = (username, email, password) => {
+        axios
+            .post("/api/users", {
+                username: username,
+                email: email,
+                password: password,
+            })
+            .then(response => {
+                this.setState({ successMessage: "Registered!" });
+                // this._toggleLoginModal();
+
+            })
+            .catch(err => console.log(err));
+    }
+
+    _onLogin = (submittedUsername, submittedPassword) => {
+        axios
+            .post("/api/auth", {
+                username: submittedUsername,
+                password: submittedPassword
+            })
+            .then(response => {
+                this.setState({
+                    username: response.data.username,
+                    id: response.data.id
+                })
+                this._toggleLoginModal();
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({ errorMessage: err });
+            });
+    }
+
+    _onLogout = () => {
+        axios
+            .delete("/api/auth")
+            .then(response => {
+                console.log(response.data);
+                this.setState({ username: '', id: '' })
+            }) // handle headers
+            .catch(err => console.log(err));
+    }
+
+    _toggleLoginModal = () => {
+        this.setState({ loginModalOpen: !this.state.loginModalOpen })
+    }
+
     render() {
         return (
             <div>
                 <Navbar className="simple-navbar" dark expand="md">
 
-                    <Button className="home-button">HOME</Button>
+                    <NavLink href={`/`}>
+                        <Button className="home-button">HOME</Button>
+                    </NavLink>
 
-                    <ProfilePanel 
-                    
+                    <ProfilePanel
+                        username={this.state.username}
+                        onLogin={this._onLogin}
+                        onRegister={this._onRegister}
+                        onLogout={this._onLogout}
+                        isLoginModalOpen={this.state.loginModalOpen}
+                        toggleLoginModal={this._toggleLoginModal}
+                        errorMessage={this.state.errorMessage}
+                        successMessage={this.state.successMessage}
                     />
                 </Navbar>
             </div>
